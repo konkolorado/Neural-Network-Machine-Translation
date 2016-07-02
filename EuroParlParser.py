ba3f546ce4b6e950b2cb6a796d23dd8d7feb571b
urielmandujano <uriel.mandujano14@gmail.com>
2016-07-02 17:14:47 -0500

    Began parser skeleton

---
+++ EuroParlParser.py
@@ -0,0 +1,21 @@
+"""
+Author: Uriel Mandujano
+Parser for eurparl data available at
+    http://www.statmt.org/europarl/
+For use with the Moses decoder
+"""
+
+class EuroParlParser(object):
+    def __init__(self):
+        return
+
+    def __string__(self):
+        return ""
+
+
+def main():
+    euro_parser = EuroParlParser()
+    assert euro_parser is not None
+
+if __name__ == '__main__':
+    main()
