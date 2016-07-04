45d8cafeaa0c579296da1b9cc3feec4aabff9a60
urielmandujano <uriel.mandujano14@gmail.com>
2016-07-04 01:18:01 -0500

    Refactored, removed churchgale alg, added vocab methods

--- EuroParlParser.py
+++ EuroParlParser.py
@@ -3,46 +3,109 @@ Author: Uriel Mandujano
 Parser for eurparl data available at
     http://www.statmt.org/europarl/
 For use with the Moses decoder
+
+NOTE:
+Moses decoder only compatible with:
+    - all words must be lowercased
+    - 1 sentence per line, no empty lines
+    - sentences no longer than 100 words
 """
 import os
 import subprocess
 
 class EuroParlParser(object):
-    def __init__(self, lang1, lang2):
-        self.lang1 = lang1
-        self.lang2 = lang2
-        self.lang_files_exist()
+    def __init__(self, lang1_dir, lang2_dir):
+        self.lang1_dir = lang1_dir
+        self.lang2_dir = lang2_dir
+        self._lang_files_exist()
+        self._load_langs()
 
     def __str__(self):
-        return "{}\n{}".format(self.lang1, self.lang2)
+        return "{}\n{}".format(self.lang1_dir, self.lang2_dir)
 
-    def lang_files_exist(self):
-        assert os.path.exists(self.lang1), \
-            "{} file not found".format(self.lang1)
-        assert os.path.exists(self.lang2), \
-            "{} file not found".format(self.lang2)
+    def _lang_files_exist(self):
+        assert os.path.exists(self.lang1_dir), \
+            "{} file not found".format(self.lang1_dir)
+        assert os.path.exists(self.lang2_dir), \
+            "{} file not found".format(self.lang2_dir)
+
+    def _load_langs(self):
+        """
+        Loads language data from files into class matrices with a
+        correspondence between indices in each matrix. Lowercases words here
+        """
+        self.lang1, self.lang2 = [], []
+        file1 = open(self.lang1_dir)
+        file2 = open(self.lang2_dir)
+        for line1, line2 in zip(file1, file2):
+            line1 = line1.strip().lower()
+            line2 = line2.strip().lower()
+            self.lang1.append(line1), self.lang2.append(line2)
+
+        assert len(self.lang1) == len(self.lang2), "Unequal language sizes"
+        assert len(self.lang1) and len(self.lang2), "Got language of size 0"
+
+    def _split_sentences(self, line):
+        """
+        Given a string, attempts to split into distinct sentences based on
+        punctuation
+        """
+        #TODO
+        #Import from NLTK
 
     def clean_corpus(self):
         """
-        Remove empty lines, remove redundant space characters, drop lines
-        that are too empty, short, or long
+        Drop lines that are empty, too short, or too long.
         """
-        command = 'clean-corpus-n.perl CORPUS L1 L2 OUT MIN MAX'
-        #TODO
+        min_line_len = 0
+        max_line_len = 100
+        pop_indices = []
+        for i, (l1, l2) in enumerate(zip(self.lang1, self.lang2)):
+            l1, l2 = l1.split(), l2.split()
+            if l1 == l2 == []:
+                pop_indices.append(i)
+            elif len(l1) <= min_line_len or len(l2) <= min_line_len:
+                pop_indices.append(i)
+            elif len(l1) > max_line_len or len(l2) > max_line_len:
+                pop_indices.append(i)
 
-    def sentence_align(self):
+        for i in pop_indices[::-1]:
+            self.lang1.pop(i), self.lang2.pop(i)
+
+    def create_vocab(self):
         """
-        Align sentences between the two corpuses using the Gale-Church
-        algorithm. Recommended to improve translation quality as sentence-to-
-        sentence translations are more reliable than paragraph to paragraph
+        Creates a dictionary containing the vocabulary of each language.
+        Keys are words, values are counts
         """
-        #TODO
+        self.vocab_lang1, self.vocab_lang2 = dict(), dict()
+        self._create_lang_vocab(self.vocab_lang1, self.lang1)
+        self._create_lang_vocab(self.vocab_lang2, self.lang2)
+
+    def _create_lang_vocab(self, vocab_lang, lang):
+        """
+        Populates language dictionary and each word entry's counts. Takes
+        as parameter a dictionary{key:word; value:count} and lang matrix
+        """
+        for sentence in lang:
+            sentence = sentence.split()
+            for word in sentence:
+                vocab_lang[word] = 1 + vocab_lang.get(word, 0)
+
+    def vocab_to_list(self):
+        """
+        Returns a tuple with two entries: a list of the vocabulary in
+        language 1 and another list of the vocabulary in language 2
+        """
+        return sorted(self.vocab_lang1.keys()), \
+               sorted(self.vocab_lang2.keys())
 
 def main():
     lang1 = '/Users/urielmandujano/data/europarl/europarl-v7.es-en.en'
     lang2 = '/Users/urielmandujano/data/europarl/europarl-v7.es-en.es'
     euro_parser = EuroParlParser(lang1, lang2)
-    print "Pass"
+    euro_parser.clean_corpus()
+    euro_parser.create_vocab()
+    print euro_parser.vocab_to_list()[0]
 
 if __name__ == '__main__':
     main()
