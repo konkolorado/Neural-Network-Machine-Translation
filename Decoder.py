6317367f5ff84a32b5d8db013f48bb502c24733c
urielmandujano <uriel.mandujano14@gmail.com>
2016-07-08 21:19:01 -0500

    Starter code for Decoder

---
+++ Decoder.py
@@ -0,0 +1,21 @@
+"""
+Author: Uriel Mandujano
+Decoder for use with Moses and the Eurparl data. Performs statistical
+MT between two pairs of languages and by pivoting.
+"""
+from EuroParlParser import EuroParlParser
+
+class Decoder(object):
+    def __init__(self, lang1_dir, lang2_1_dir, lang2_2_dir ,lang3_dir):
+        parser1 = EuroParlParser(lang1_dir, lang2_1_dir)
+        parser2 = EuroParlParser(lang2_2_dir, lang3_dir)
+
+def main():
+    lang1 = '/Users/urielmandujano/data/europarl/europarl-v7.es-en.es'
+    lang2_1 = '/Users/urielmandujano/data/europarl/europarl-v7.es-en.en'
+    lang2_2 = '/Users/urielmandujano/data/europarl/europarl-v7.fr-en.en'
+    lang3 = '/Users/urielmandujano/data/europarl/europarl-v7.fr-en.fr'
+    decoder = Decoder(lang1, lang2_1, lang2_2, lang3)
+
+if __name__ == "__main__":
+    main()
